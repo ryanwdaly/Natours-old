22fc477dde6ddb26f8d20b096fce5287b1334aea
ryanwdaly <ryan.w.daly90@gmail.com>
2022-04-06 12:19:38 -0400

    refactored api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,68 +1,32 @@
-//const fs = require('fs');
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
-//exports.checkID = (req, res, next, val) => {
-//  console.log(`Tour id is: ${val}`);
-//  if (req.params.id * 1 > tourList.length) {
-//    return res.status(404).json({
-//      status: 'fail',
-//      message: 'Invalid ID',
-//    });
-//  }
-//  next();
-//};
-//
-//exports.checkBody = (req, res, next) => {
-//  if (!req.body.name || !req.body.price) {
-//    return res.status(400).json({
-//      status: 'fail',
-//      message: 'no name and/or pice',
-//    });
-//  }
-//  next();
-//};
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summery,difficulty';
+  next();
+};
 
 ////////////////////////ROUTE HANDLERS///////////////////////////////
 // VIEW ALL TOURS
 exports.getAllTours = async (req, res) => {
   try {
-    // BUILD QUERY
-    // 1A) Filtering
-    const queryObj = {...req.query}; //restucture so req.query does not change
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
-
-    // 1B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-    let query =  Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    if(req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3) Field limiting
-    if(req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields)
-    } else {
-      query = query.select('-__v');
-    }
-    
     // EXECUTE QUERY
-    const tours = await query
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
       data: {
-        tours
+        tours,
       },
     });
   } catch (err) {
@@ -124,7 +88,6 @@ exports.createTour = async (req, res) => {
 // UPDATING WITH PATCH
 exports.updateTour = async (req, res) => {
   try {
-
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
@@ -133,31 +96,31 @@ exports.updateTour = async (req, res) => {
     res.status(200).json({
       status: 'success',
       data: {
-        tour  
-      }
+        tour,
+      },
     });
-  } catch(err) {
-      res.status(400).json({
-        status: 'fail',
-        message: 'invalid data sent!'
-      })
-    }
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'invalid data sent!',
+    });
+  }
 };
 
 ////////////////////////////////////////////////////////////////////
 // DELETE
 exports.deleteTour = async (req, res) => {
   try {
-    await Tour.findByIdAndDelete(req.params.id)
+    await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
       status: 'success',
       data: null,
     });
-  } catch(err) {
+  } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'invalid data sent!'
-    })
+      message: 'invalid data sent!',
+    });
   }
 };
 /////////////////////////////////////////////////////////////////////
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,8 @@ const router = express.Router();
 // MIDDLEWARE
 //router.param('id', tourController.checkID);
 
+router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+
 router
     .route('/')
     .get(tourController.getAllTours)
